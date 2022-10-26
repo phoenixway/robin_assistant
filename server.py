fbeeb2e8ec586cbc774d33cd5d22da7e7b3aba97
Roman Kozak <cossack.roman@gmail.com>
2022-10-27 01:15:54 +0300

    classes, etc

--- server.py
+++ server.py
@@ -8,98 +8,56 @@ from icecream import ic
 from multiprocessing import Process, Queue
 db = shelve.open('spam')
 #del db['dayplanned']
+import websockets
 
-clients = []
-messanger = None
-server_global = None
+MODULES = dict()
 
-def say(message):
-    for client in clients:
-        client.send(message)
-        ic()
+no_error = True
 
-class JustChat(WebSocket):
-    def broadcast(self, message):
-        for client in clients:
-            client.send_message(message)
+class Ws_server:
+    async def say(self, message):
+        global no_error
+        if no_error and self.websocket:
+            try:
+                await self.websocket.send(message)
+                print(f">>> {message}")
+            except websockets.exceptions.ConnectionClosedOK:
+                ic('ConnectionClosedOK')
+                no_error = False
 
-    def say(self, message):
-        for client in clients:
-            client.send_message(message)
+    async def echo(self, websocket):
+        global no_error, MODULES
+        self.websocket = websocket
+        while no_error:
+            try:
+                name = await websocket.recv()
+                print(f"<<< {name}")
+                greeting = f"Hello {name}!"
+                await websocket.send(greeting)
+                print(f">>> {greeting}")
+            except websockets.exceptions.ConnectionClosedOK:
+                ic('ConnectionClosedOK')
+                no_error = False
 
-    def handle(self):
-        for client in clients:
-            # if client != self:
-            if self.data == 'day planned':
-                dt = datetime.today().strftime('%Y-%m-%d')
-                db['dayplanned'] = dt   
-            answer = f"Echoing {self.data}"
-            client.send_message(answer)
-            # client.send_message(self.address[0] + u' - ' + self.data)
-            print(self.address[0] + u'> ' + self.data)
-            print(f"Me> {answer}")
+    async def ws_handle(self):
+        await websockets.serve(self.echo, "localhost", 8765)
+        await asyncio.Future()  # run forever
 
-    def connected(self):
-        print(f"{self.address[1]} process from {self.address[0]} is connected. ")
-        for client in clients:
-            client.send_message(self.address[0] + u' - connected')
-        clients.append(self)
-        global messanger 
-        messanger = self
-        pass
+class Watcher:
+    async def watch(self):
+        while True:
+            await asyncio.sleep(10)
+            ic("im watch")
+            await MODULES['ws_server'].say('Tada!')
 
-    def handle_close(self):
-        clients.remove(self)
-        print(self.address, 'closed')
-        for client in clients:
-            client.send_message(self.address[0] + u' - disconnected')
+watcher = Watcher()
+ws_server = Ws_server()
 
-
-def ws_handler(queue):
-    ic('server connected')
-    server = WebSocketServer('', 8765, JustChat)
-    queue.put(server)
-    server.serve_forever()
-
-async def queue_handler():
-    pass
-
-def watch_params(queue):
-    print('watch_params')
-    while True:
-        sleep(10)
-        dt = datetime.today().strftime('%Y-%m-%d')
-        if not db.get('dayplanned') or db.get('dayplanned') != dt:
-            m = "You did not plan your day!"
-            ic(m)
-            server = queue.get()
-
-            # ic(server.clients)
-            say(m)
-            ic()
+MODULES['watcher'] = watcher
+MODULES['ws_server'] = ws_server
 
 async def main():
-    #task1 = asyncio.create_task(ws_handler())
-    #task2 = asyncio.create_task(queue_handler())
-    task3 = asyncio.create_task(watch_params())
-    await task3
-    # await task1
-    # #await task2
-    # await task3
-q = Queue()
-processes = []
-proc = Process(target=ws_handler, args=(q,))
-proc.start()
-processes.append(proc)
-proc1 = Process(target=watch_params, args=(q,))
-proc1.start()
-processes.append(proc1)
-try:
-    #asyncio.run(main())
-    pass
-except KeyboardInterrupt:
-    pass
-finally:
-    db.close()
-    for proc in processes:
-        proc.join()
+    results = await asyncio.gather(ws_server.ws_handle(), watcher.watch())
+    ic(results)
+
+asyncio.run(main())
