d714637cf75ab9b4aaf02e86779dab9e9cd38dfe
Roman Kozak <cossack.roman@gmail.com>
2022-10-26 03:42:15 +0300

    server works

--- server.py
+++ server.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python3
-
 import asyncio
-import websockets
+from simple_websocket_server import WebSocketServer, WebSocket
 
-async def echo(websocket):
-    async for message in websocket:
-        print(f"\n> {message}")
-        await websocket.send(message)
+class SimpleChat(WebSocket):
+    def handle(self):
+        for client in clients:
+            # if client != self:
+            answer = f"Echoing {self.data}"
+            client.send_message(answer)
+            # client.send_message(self.address[0] + u' - ' + self.data)
+            print(self.address[0] + u'> ' + self.data)
+            print(f"Me> {answer}")
 
-async def websocket_handler():
-    async with websockets.serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+    def connected(self):
+        print(f"{self.address[1]} process from {self.address[0]} is connected. ")
+        for client in clients:
+            client.send_message(self.address[0] + u' - connected')
+        clients.append(self)
 
+    def handle_close(self):
+        clients.remove(self)
+        print(self.address, 'closed')
+        for client in clients:
+            client.send_message(self.address[0] + u' - disconnected')
+
+
+clients = []
+
+async def ws_handler():
+    server = WebSocketServer('', 8765, SimpleChat)
+    server.serve_forever()
+
+async def queue_handler():
+    pass
 
 async def main():
-    task_ws = asyncio.create_task(websocket_handler())
-    await task_ws
+    task1 = asyncio.create_task(ws_handler())
+    task2 = asyncio.create_task(queue_handler())
+    await task1
+    await task2
 
-asyncio.run(main())
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    pass
