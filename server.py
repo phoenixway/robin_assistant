47a4b0fd0a2ca8a4c453e2c7daaaac77ce78e73d
Roman Kozak <cossack.roman@gmail.com>
2022-10-27 03:42:42 +0300

    modules

--- server.py
+++ server.py
@@ -1,63 +1,34 @@
 #!/usr/bin/env python3
 import asyncio
 import shelve
-from time import sleep
-from simple_websocket_server import WebSocketServer, WebSocket
 from datetime import datetime
 from icecream import ic
-from multiprocessing import Process, Queue
-db = shelve.open('spam')
-#del db['dayplanned']
 import websockets
+import logging
+from termcolor import colored
+from colorlog import ColoredFormatter
+from ws_server import Ws_server
+from watcher import Watcher
 
+log = logging.getLogger('pythonConfig')
 MODULES = dict()
 
-no_error = True
-
-class Ws_server:
-    async def say(self, message):
-        global no_error
-        if no_error and self.websocket:
-            try:
-                await self.websocket.send(message)
-                print(f">>> {message}")
-            except websockets.exceptions.ConnectionClosedOK:
-                ic('ConnectionClosedOK')
-                no_error = False
-
-    async def echo(self, websocket):
-        global no_error, MODULES
-        self.websocket = websocket
-        while no_error:
-            try:
-                name = await websocket.recv()
-                print(f"<<< {name}")
-                greeting = f"Hello {name}!"
-                await websocket.send(greeting)
-                print(f">>> {greeting}")
-            except websockets.exceptions.ConnectionClosedOK:
-                ic('ConnectionClosedOK')
-                no_error = False
-
-    async def ws_handle(self):
-        await websockets.serve(self.echo, "localhost", 8765)
-        await asyncio.Future()  # run forever
-
-class Watcher:
-    async def watch(self):
-        while True:
-            await asyncio.sleep(10)
-            ic("im watch")
-            await MODULES['ws_server'].say('Tada!')
-
-watcher = Watcher()
+watcher = Watcher(MODULES)
 ws_server = Ws_server()
+db = shelve.open('spam')
+#del db['dayplanned']
 
 MODULES['watcher'] = watcher
 MODULES['ws_server'] = ws_server
+MODULES['db'] = db
 
 async def main():
     results = await asyncio.gather(ws_server.ws_handle(), watcher.watch())
     ic(results)
 
-asyncio.run(main())
+log.info('Welcome!')
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    ic('Bye')
