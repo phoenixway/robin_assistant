a252b53af499219ce725699b7cb43d22bab73f18
Roman Kozak <cossack.roman@gmail.com>
2022-11-07 23:51:16 +0200

    new stories format works!

--- ai_core.py
+++ ai_core.py
@@ -18,7 +18,7 @@ intents = []
 def recognize_intent(text):
     for intent in intents:
         text = text.lower()
-        m = re.search(intent, text)
+        m = re.match(intent, text)
         if m is not None:
             return intents[intent]
 
@@ -35,7 +35,6 @@ def init_brains():
     global intents
     with open(source_dir/'brains/default.stories', 'r') as f:
         stories_ = json.load(f)
-
     with open(source_dir/'brains/default.intents', 'r') as f:
         intents = json.load(f)
 
@@ -81,7 +80,6 @@ def way_to_beginning(lst, val):
     return result, is_collection
     
 def make_canonical(data):
-    result = []
     linear_way = []
     ways = []
     if isinstance(data, list):
@@ -117,87 +115,6 @@ def make_canonical(data):
         return ways, True
     else:
         return linear_way, False
-        
-def make_canonical_old(lst):
-    result = []
-    linear_way = []
-    ways = []
-    for item in lst:
-        if isinstance(item, str):
-            if len(ways) > 0:
-                for w in ways:
-                    w.append(item)
-            else:
-                linear_way.append(item)
-        elif isinstance(item, dict):
-            branches = []
-            for it in item:
-                if isinstance(item[it], list):
-                    l, _ = make_canonical(item[it])
-                    branches.append([it] + l)
-                else:
-                    branches.append([it, item[it]])
-            for b in branches:
-                lw = deepcopy(linear_way)
-                lw.extend(b)
-                ways.append(lw)
-    if len(ways) > 0:
-        return ways, True
-    else:
-        return linear_way, False
-    
-def make_canonical_old(lst_to_process):
-    result = []
-    cur_lineay_way = []
-    cur_ways = []
-    done = False
-    is_collection = False
-    for item in lst_to_process:
-        if done:
-            break
-        if isinstance(item, str):
-            if len(cur_ways) > 0:
-                cur_ways = [cw + [item] for cw in cur_ways ]
-            else:
-                cur_lineay_way.append(item)
-        elif isinstance(item, list) and not any(isinstance(item, str) for item in item):
-            ways = []
-            # ways_is_collection = False
-            for item_sub_list in item:
-                ways_is_collection = False
-                res, is_collection = make_canonical(item_sub_list)
-                if is_collection and not ways_is_collection:
-                    ways = res
-                    ways_is_collection = True
-                elif not is_collection and ways_is_collection:
-                    for way in ways:
-                        way.extend(res)
-                else:
-                    ways.append(res)
-
-            if len(cur_ways) > 0:
-                new_ways = []
-                for cw in cur_ways:
-                    for way in ways:
-                        ll = deepcopy(cw)
-                        ll.extend(way)
-                        new_ways.append(ll)
-                cur_ways = new_ways
-            else:
-                new_ways = []
-                for way in ways:
-                        base = deepcopy(cur_lineay_way) 
-                        base.extend(way)
-                        new_ways.append(base)
-                cur_ways = new_ways
-
-    if len(cur_ways) > 0:
-        result = cur_ways
-        is_collection = True
-    else:
-        result = cur_lineay_way    
-        is_collection = False            
-    return result, is_collection
 
 #TODO: study it
 def contains(subseq, inseq):
@@ -213,6 +130,8 @@ def contains_in_end(subseq, inseq):
 
 def is_story_in_log(story, l):
     result = False, -1
+    # key = (story.keys())[0]
+    # way, is_collection = story[key], True
     way, is_collection = way_to_beginning(story, l[-1])
     if len(way) == 0:
         return result
@@ -222,48 +141,41 @@ def is_story_in_log(story, l):
         i = len(story)
         while i >= 0:
             if contains_in_end(story[0:i], l):
-                return True, story, i
+                return True, i
             # elif contains_in_end(story[0:i], l):
             #     return True, i
             i = i - 1
-    elif len(l) < len(story):
+    elif len(l) <= len(story):
         i = len(l)
         while i >= 0:
             if contains_in_end(story[0:i], l):
                 return True, i
             i = i - 1
     return result
-
-def is_story_in_log_old(story, l):
-    result = False, -1
-    if len(l) > len(story):
-        i = len(story)
-        while i >= 0:
-            if contains_in_end(story[0:i], l):
-                return True, i
-            # elif contains_in_end(story[0:i], l):
-            #     return True, i
-            i = i - 1
-    elif len(l) < len(story):
-        i = len(l)
-        while i >= 0:
-            if contains_in_end(story[0:i], l):
-                return True, i
-            i = i - 1
-    return result
-
-
-
 
 SILENCE_TIME = 10
 
 class AICore:
-    stories = {}
+    def canonize(stories):
+        final = {}
+        for s in stories:
+            story_body = stories[s]
+            branches, has_branches = make_canonical(story_body)
+            if has_branches:
+                i = 0
+                
+                for b in branches:
+                    final[f"{s}::{i}"] = b
+                    i = i + 1
+            else:
+                final[s] = stories[s]
+        return final
+
     def __init__(self, modules):
         # create_default_stories()
         #TODO: check that brains exist
         init_brains()
-        AICore.stories = stories_
+        self.stories = AICore.canonize(stories_)
         self.modules = modules
         self.active_story = None
         self.log = []
@@ -272,7 +184,16 @@ class AICore:
         self.silence_task = None
         self.is_started = True
         self.repeat_if_silence = False
-        self.robins_stories = ['robin_asks']
+        self.robins_stories = ['trees::0']
+
+    def get_story(self, key):
+        s = self.stories.get(key, None)
+        if s is None:
+            keys = self.stories.keys()
+            key2 = [k for k in keys if k.startswith(key)]
+            s = self.stories[key2[0]]
+        return s 
+
 
     async def wait_to_talk(self, s):
         await asyncio.sleep(SILENCE_TIME)
@@ -299,16 +220,19 @@ class AICore:
         answer = f"Default answer on '{text}'"
         try:
             intent = recognize_intent(text)
-            self.log.append(f"<intent>{intent}")
+            if intent is not None:
+                self.log.append(f"<intent>{intent}")
+            else:
+                self.log.append(text)
             #FIXME: take any input
             if intent:
-                for story_id in AICore.stories:
-                    is_active_story, story, pos = is_story_in_log(AICore.stories[story_id], self.log)    
+                for story_id in self.stories:
+                    is_active_story, pos = is_story_in_log(self.stories[story_id], self.log)    
                     if is_active_story:
                         ##FIXME
-                        next_answer = AICore.stories[story_id][pos]
+                        next_answer = self.stories[story_id][pos]
                         if next_answer.startswith('<func>'):
-                            fn_name = (AICore.stories[story_id][pos])[6:]
+                            fn_name = (self.stories[story_id][pos])[6:]
                             fn = getattr(actions.default, fn_name)
                             #TODO: get rid of sending modules each time fn calls
                             answer = fn(self.modules)
@@ -317,6 +241,15 @@ class AICore:
                             answer = next_answer
                             self.log.append(next_answer)
                         break
+            else:
+                for story_id in self.stories:
+                    is_active_story, pos = is_story_in_log(self.stories[story_id], self.log)    
+                    if is_active_story:
+                        ##FIXME
+                        next_answer = self.stories[story_id][pos]
+                        answer = next_answer
+                        self.log.append(next_answer)
+                        break
         except Exception as e:
             answer = f"Error happened in ai_core.parse(): {e}"
         return answer
@@ -324,10 +257,10 @@ class AICore:
     def start_story(self, story_id):
         #FIXME:whole func
         log.debug("Make story start by Robin's will")
-        if story_id in stories and len(stories[story_id]) > 0:
-            next_answer = stories[story_id][0]
+        if story_id in self.stories and len(self.stories[story_id]) > 0:
+            next_answer = self.stories[story_id][0]
             if next_answer.startswith('<func>'):
-                fn_name = (stories[story_id][0])[6:]
+                fn_name = (self.stories[story_id][0])[6:]
                 fn = getattr(actions.default, fn_name)
                 #TODO: get rid of sending modules each time fn calls
                 answer = fn(self.modules)
--- tests/test_aicore.py
+++ tests/test_aicore.py
@@ -1,21 +1,25 @@
 from .. import ai_core
+import ai_core
 from ai_core import contains_in_end, AICore
 from ..actions import default
 import copy
-
+from pprint import pprint
 
 def test_aicore():
+    ai_core.init_brains()
     aicore = AICore(None)
-    s = AICore.stories['trees']
+    assert len(ai_core.stories_) > 0, 'There should be at least one story'
+    s = ai_core.stories_.get('trees', None)
+    assert s is not None, 'cant access to trees story'
     l = ["Do u know yr main priorities for today?", "<intent>yes"]
-    assert isinstance(AICore.stories['trees'], list), 'stories[tree] exception'
+    assert isinstance(ai_core.stories_['trees'], list), 'stories[tree] exception'
     assert aicore is not None, 'aicore creation failed'
 
 
 def test_make_canonical_simple():
     lst = ['a0', 'a1', 'b2', 'a3', 'b4']
     res, is_collection = ai_core.make_canonical(lst)
-    assert is_collection == False, 'fail'
+    assert is_collection == False, 'must be not a collection'
     assert len(res) == 5, 'fail'
     needed = ['a0', 'a1', 'b2', 'a3', 'b4']
     result = all(r == n for r, n in zip(res, needed))
@@ -60,33 +64,63 @@ def test_make_canonical_3():
     result = all(r == n for r, n in zip(res[2], needed))
     assert result, '3 list is wrong'
 
-def test_make_canonical_hard1():
-    lst = ['in1', 'out2', 'in3', 'out4', [
-        ['in5', 'out6'], ['ii1', 'oo2']], 'in9', 'out10']
-    res, is_collection = ai_core.make_canonical(lst)
-    assert is_collection == True, 'not collection althrough it must be'
-    assert len(res) == 2, 'fail'
-    needed = ['in1', 'out2', 'in3', 'out4', 'in5', 'out6', 'in9', 'out10']
-    result = all(r == n for r, n in zip(res[0], needed))
-    assert result, 'first list is wrong'
-    needed = 'in1', 'out2', 'in3', 'out4', 'ii1', 'oo2', 'in9', 'out10'
-    result = all(r == n for r, n in zip(res[1], needed))
-    assert result, 'second list is wrong'
+# def test_make_canonical_hard1():
+#     lst = ['in1', 'out2', 'in3', 'out4', [
+#         ['in5', 'out6'], ['ii1', 'oo2']], 'in9', 'out10']
+#     res, is_collection = ai_core.make_canonical(lst)
+#     assert is_collection == True, 'not collection althrough it must be'
+#     assert len(res) == 2, 'fail'
+#     needed = ['in1', 'out2', 'in3', 'out4', 'in5', 'out6', 'in9', 'out10']
+#     result = all(r == n for r, n in zip(res[0], needed))
+#     assert result, 'first list is wrong'
+#     needed = 'in1', 'out2', 'in3', 'out4', 'ii1', 'oo2', 'in9', 'out10'
+#     result = all(r == n for r, n in zip(res[1], needed))
+#     assert result, 'second list is wrong'
+# def test_make_canonical_hard2():
+#     lst = ['in1', 'out2', 'in3', 'out4', [
+#         ['in5', 'out6', [['f1', 'f2'], ['f3', 'f4']]], 
+#         ['ii1', 'oo2']
+#     ], 'in9', 'out10']
+#     res, is_collection = ai_core.make_canonical(lst)
+#     assert is_collection == True, 'not collection althrough it must be'
+#     assert len(res) == 2, 'fail'
+#     needed = ['in1', 'out2', 'in3', 'out4',
+#               'in5', 'out6', 'f1', 'f2', 'in9', 'out10']
+#     result = all(r == n for r, n in zip(res[0], needed))
+#     assert result, 'first list is wrong'
+#     needed = ['in1', 'out2', 'in3', 'out4',
+#               'in5', 'out6', 'f3', 'f4', 'in9', 'out10']
+#     result = all(r == n for r, n in zip(res[1], needed))
+#     assert result, 'second list is wrong'
 
+def test_canonize():
+    aicore = AICore(None)
+    s = ai_core.stories_['trees']
+    assert s is not None, 'no story'
+    res = AICore.canonize({'trees': s})
+    pprint(res)
+    assert res is not None, 'no canonized stories'
+    assert len(res) == 3, 'expected 3 canonized stories'
+    key = (list(res.keys()))[0]
+    assert res[key]
 
-def test_make_canonical_hard2():
-    lst = ['in1', 'out2', 'in3', 'out4', [
-        ['in5', 'out6', [['f1', 'f2'], ['f3', 'f4']]], 
-        ['ii1', 'oo2']
-    ], 'in9', 'out10']
-    res, is_collection = ai_core.make_canonical(lst)
-    assert is_collection == True, 'not collection althrough it must be'
-    assert len(res) == 2, 'fail'
-    needed = ['in1', 'out2', 'in3', 'out4',
-              'in5', 'out6', 'f1', 'f2', 'in9', 'out10']
-    result = all(r == n for r, n in zip(res[0], needed))
-    assert result, 'first list is wrong'
-    needed = ['in1', 'out2', 'in3', 'out4',
-              'in5', 'out6', 'f3', 'f4', 'in9', 'out10']
-    result = all(r == n for r, n in zip(res[1], needed))
-    assert result, 'second list is wrong'
+def test_story_in_log_simple():
+    aicore = AICore(None)
+    s = aicore.stories['greetings']
+    assert s is not None, 'no story'
+    log = ['<intent>greetings']
+    res, i = ai_core.is_story_in_log(s, log)
+    assert res == True, 'story is not in log'
+    assert i == 1, 'wrong next statement index'
+
+def test_parse():
+    aicore = AICore(None)
+    s = aicore.get_story('trees')
+    assert s is not None, 'no story'
+    l = ["Do u know yr main priorities for today?", "<intent>yes"]
+    assert isinstance(s, list), 'stories[tree] exception'
+    assert aicore is not None, 'aicore creation failed'
+    assert aicore.stories is not None, 'stories creation failed'
+    text = 'Hey'
+    res = aicore.parse(text)
+    assert res is not None, 'parse exception'
