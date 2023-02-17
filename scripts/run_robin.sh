d2db8ffec5679a2dcc62e2e804fe7e917a4b7648
Roman Kozak <cossack.roman@gmail.com>
2023-02-17 14:09:20 +0200

    new user setup bugfixing

--- scripts/run_robin.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-~/Projects/robin_assistant/ws_chat/dist/ws_chat/ws_chat-linux_x64 & ~/Projects/robin_assistant/robin.py && fg
---
+++ scripts/run_robin_gui.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+$DIR/ws_chat/dist/ws_chat/ws_chat-linux_x64 & $DIR/../src/robin.py && fg
